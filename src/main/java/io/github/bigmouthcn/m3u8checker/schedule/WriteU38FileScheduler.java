9b2895ebc285a0865173835acd96813f39d5bcbc
Allen Hu <allen.hu@bianxianmao.com>
2024-11-22 14:43:28 +0800

    add log

--- src/main/java/io/github/bigmouthcn/m3u8checker/schedule/WriteU38FileScheduler.java
+++ src/main/java/io/github/bigmouthcn/m3u8checker/schedule/WriteU38FileScheduler.java
@@ -1,6 +1,7 @@
 package io.github.bigmouthcn.m3u8checker.schedule;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IORuntimeException;
 import io.github.bigmouthcn.m3u8checker.checker.M3u8;
 import io.github.bigmouthcn.m3u8checker.database.InfoService;
 import lombok.extern.slf4j.Slf4j;
@@ -26,21 +27,26 @@ public class WriteU38FileScheduler implements Scheduler {
     @Override
     @Scheduled(cron = "0 */10 * * * ?")
     public void schedule() {
-        StringBuilder allChannel = new StringBuilder();
-        allChannel.append("#EXTM3U").append("\n");
+        try {
+            StringBuilder allChannel = new StringBuilder();
+            allChannel.append("#EXTM3U").append("\n");
 
-        StringBuilder justOk = new StringBuilder();
-        justOk.append("#EXTM3U").append("\n");
+            StringBuilder justOk = new StringBuilder();
+            justOk.append("#EXTM3U").append("\n");
 
-        List<M3u8> all = infoService.findAll();
-        for (M3u8 m3u8 : all) {
-            String line = m3u8.convert2M3u8Line();
-            if (m3u8.isOk()) {
-                justOk.append(line).append("\n");
+            List<M3u8> all = infoService.findAll();
+            for (M3u8 m3u8 : all) {
+                String line = m3u8.convert2M3u8Line();
+                if (m3u8.isOk()) {
+                    justOk.append(line).append("\n");
+                }
+                allChannel.append(line).append("\n");
             }
-            allChannel.append(line).append("\n");
+            FileUtil.writeString(allChannel.toString(), "iptv.m3u8", "UTF-8");
+            FileUtil.writeString(justOk.toString(), "iptv-ok.m3u8", "UTF-8");
+            log.info("WriteU38FileScheduler success");
+        } catch (Exception e) {
+            log.error("WriteU38FileScheduler error", e);
         }
-        FileUtil.writeString(allChannel.toString(), "iptv.m3u8", "UTF-8");
-        FileUtil.writeString(justOk.toString(), "iptv-ok.m3u8", "UTF-8");
     }
 }
